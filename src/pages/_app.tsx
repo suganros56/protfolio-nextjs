8fea179394531ad8db8177232c238ac3f519699a
Roshan Suganthan <suganros52@gmail.com>
2023-03-07 23:54:38 +0530

    added work, exp, about pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,9 @@
+import Navbar from '@/components/layouts'
+import Footer from '../components/footer'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
+ 
 }
